1a95cf5732fb046ba4460ae7a31b83baddba8f22
Jen Chartier <JChartier001@gmail.com>
2021-02-26 08:15:25 -0600

    login register routes

--- server/app.js
+++ server/app.js
@@ -8,6 +8,7 @@ const config = require('./config/default');
 const flash = require('connect-flash');
 const port = 333;
 const app = express();
+const passport = require('./auth/passport')
 
 
 mongoose.connect('mongodbL//127.0.0.1/nodStream', { useNewUrlParser: true});
@@ -20,7 +21,8 @@ app.use(flash())
 app.use(require('cookie-parser')());
 app.use(bodyParse.urlencoded({extended:true}));
 app.use(bodyParse.json({extended: true}));
-
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(Session({
     store: new FileStore({
         path : './server/sessions'
@@ -28,6 +30,9 @@ app.use(Session({
     secret: config.server.secrey,
     maxAge : Date().now + (60 * 1000 * 30)
 }));
+
+app.use('/login', require('./routes/login'));
+app.use('/register', require('./routes/register'));
 
 app.get('*', middlware.ensureLoggedIn(), (req, res) =>{
     res.render('index')
