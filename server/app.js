f7d7cb87c07ab0c5ab18a7ba0b7246a42b5625c7
Jen Chartier <JChartier001@gmail.com>
2021-02-26 07:40:23 -0600

    installed dependencies.  set up server

---
+++ server/app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const Session = require('express-session');
+const bodyParse = require('body-parser');
+const mongoose = require('mongoose');
+const middleware = require('connect-ensure-login');
+const FileStore = require('session-file-store')(Session);
+const config = require('./config/default');
+const flash = require('connect-flash');
+const port = 333;
+const app = express();
+
+
+mongoose.connect('mongodbL//127.0.0.1/nodStream', { useNewUrlParser: true});
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, './views'));
+app.use(express.static('public'));
+app.use(flash())
+;
+app.use(require('cookie-parser')());
+app.use(bodyParse.urlencoded({extended:true}));
+app.use(bodyParse.json({extended: true}));
+
+app.use(Session({
+    store: new FileStore({
+        path : './server/sessions'
+    }),
+    secret: config.server.secrey,
+    maxAge : Date().now + (60 * 1000 * 30)
+}));
+
+app.get('*', middlware.ensureLoggedIn(), (req, res) =>{
+    res.render('index')
+})
+
+app.listen(port, () => console.log(`App listening on ${port}!`));
